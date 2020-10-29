9b74ff1e55f454eb33c210226241faff92e89eed
Student <hoangsinguyen1058@gmail.com>
2020-10-29 11:32:06 +0700

    finish initial new product

--- app/src/main/java/com/example/customrecylerview/MainActivity.java
+++ app/src/main/java/com/example/customrecylerview/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.customrecylerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView rclProduct;
+    private ProductCustomAdapter productCustomAdapter;
+    private ArrayList<Product> productArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initial product array list
+        productArrayList = new ArrayList<>();
+        //add item to productArrayList
+        productArrayList.add(
+                new Product("Dây cuốn", R.drawable.bluecable, 12000, 25, 4, 15));
+        productArrayList.add(
+                new Product("Dây cuốn", R.drawable.bluecable, 25000, 14, 5, 20));
+        productArrayList.add(
+                new Product("Dây cuốn", R.drawable.bluecable, 30000, 39, 3, 25));
+        rclProduct = findViewById(R.id.rclMain);
     }
 }
